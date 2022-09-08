e31be042ae0d734ab3b7f4ebbacc55906fc345fb
Hedgebzh <axel.gore35@gmail.com>
2022-09-08 10:00:49 +0200

    task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-hexadecimal = ''
+
 for idx in range(0, 100):
-    if idx < 10:
-        print('0{}, '.format(idx), end="")
     if idx < 99:
-        print('{}, '.format(idx), end="")
-    else:
-        print('{}'.format(idx), end="\n")
+        print('{:02}'.format(idx), end=", ")
+    if idx == 99:
+        print('{}'.format(idx))
