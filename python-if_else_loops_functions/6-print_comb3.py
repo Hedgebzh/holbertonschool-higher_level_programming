1c4e48feb0630ab64b56ce9f50deb3004563c461
Hedgebzh <axel.gore35@gmail.com>
2022-09-08 14:09:34 +0200

    task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 for idx in range(0, 9):
-    for idx2 in range(idx + 1, 10): #se servir de l'index de la précédente boucle
+    for idx2 in range(idx + 1, 10):
         if idx == 8 and idx2 == 9:
             print(f'{idx}{idx2}'.format())
         else:
             print(f'{idx}{idx2}'.format(), end=", ")
-
-
