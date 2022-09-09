7e545c126fb62d94e966c76965e3ab69d8b93f98
Hedgebzh <axel.gore35@gmail.com>
2022-09-09 10:08:39 +0200

    task 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def fizzbuzz():
+    for index in range(1, 101):
+        if index % 15 == 0:
+            print('FizzBuzz', end=" ")
+        elif index % 5 == 0:
+            print('Buzz', end=" ")
+        elif index % 3 == 0:
+            print('Fizz', end=" ")
+        else:
+            print('{}'.format(index), end=" ")
