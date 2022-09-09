a9cd12005125eb874b8a975a1cf09aac47196e7c
Hedgebzh <axel.gore35@gmail.com>
2022-09-09 14:22:24 +0200

    task 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    add_result = 0
+    n = len(sys.argv)
+    for index in range(1, n):
+        add_result += int(sys.argv[index])
+    print('{}'.format(add_result), end="")
