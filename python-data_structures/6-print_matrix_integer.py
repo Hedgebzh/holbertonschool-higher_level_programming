698ebbf3029fb5befebc997ebfe2b9b7bb022c3c
Hedgebzh <axel.gore35@gmail.com>
2022-09-12 16:23:48 +0200

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
-        for idx in matrix[i]:
-            print("{:d}".format(idx), end=" ")
-        print()
+        for idx in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][idx]), end="")
+            if idx != (len(matrix[i]) - 1):
+                print(" ", end="")
+        print("")
+
