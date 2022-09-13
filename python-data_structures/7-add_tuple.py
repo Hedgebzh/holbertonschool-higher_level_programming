b9b6ccd5e22b0a939a96cc00109493173f13ba44
Hedgebzh <axel.gore35@gmail.com>
2022-09-13 15:45:26 +0200

    fix task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,16 +1,6 @@
-#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    #tuple_add = tuple(i + j for i in tuple_a for j in tuple_b)
-    #return(tuple_add)
+    a, b = len(tuple_a), len(tuple_b)
+    tuples_add = ((tuple_a[0] if a >= 1 else 0) + (tuple_b[0] if b >= 1 else 0),
+    (tuple_a[1] if a >= 2 else 0) + (tuple_b[1] if b >= 2 else 0))
+    return(tuples_add)
 
-    if len(tuple_a) or len(tuple_b) < 2:
-        for i in tuple_a:
-            if i == "":
-                i = 0
-        for j in tuple_b:
-            if i == "":
-                j = 0
-
-
-    new_tuple = tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
-    return(new_tuple)
