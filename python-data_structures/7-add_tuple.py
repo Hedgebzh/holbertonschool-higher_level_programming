755bf2e398106ba344caafcbe9304bfaae3b913d
Hedgebzh <axel.gore35@gmail.com>
2022-09-13 11:10:38 +0200

    task 9

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    #tuple_add = tuple(i + j for i in tuple_a for j in tuple_b)
+    #return(tuple_add)
+
+    if len(tuple_a) or len(tuple_b) < 2:
+        for i in tuple_a:
+            if i == "":
+                i = 0
+        for j in tuple_b:
+            if i == "":
+                j = 0
+
+
+    new_tuple = tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
+    return(new_tuple)
--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_value = max(my_list)
+    if len(my_list) == 0:
+        return None
+    else:
+        max_value = my_list[0]
+        for i in my_list:
+            if i > max_value:
+                max_value = i
     return(max_value)
