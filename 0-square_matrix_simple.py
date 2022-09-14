fbb696c4fb27cf68d27d7a6ff389b643bb889811
Hèdge <105491625+Hedgebzh@users.noreply.github.com>
2022-09-14 13:09:40 +0200

    Delete 0-square_matrix_simple.py

--- 0-square_matrix_simple.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_list = []
-    for i in range(len(matrix)):
-        new_sub_list = []
-        for idx in range(len(matrix[i])):
-            new_sub_list.append(matrix[i][idx] ** 2)
-        new_list.append(new_sub_list)
-    return (new_list)
