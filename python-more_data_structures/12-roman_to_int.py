8adc057102b9dd8191b56fbcde4657c16f99ebba
Hedgebzh <axel.gore35@gmail.com>
2022-09-15 15:36:37 +0200

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     romdic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    res = 0
-
+    r = 0
+    if roman_string >= 0 or roman_string is None:
+        return None
     for i in range(len(roman_string)):
         if i > 0 and romdic[roman_string[i]] > romdic[roman_string[i - 1]]:
-                res += romdic[roman_string[i]] - 2 * romdic[roman_string[i - 1]]
+                r += romdic[roman_string[i]] - 2 * romdic[roman_string[i - 1]]
         else:
-            res += romdic[roman_string[i]]
-    return (res)
+            r += romdic[roman_string[i]]
+    return (r)
