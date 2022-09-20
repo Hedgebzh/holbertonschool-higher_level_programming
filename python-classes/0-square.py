00eb8567afe42444404ba48fda575b3fb50375d6
Hedgebzh <axel.gore35@gmail.com>
2022-09-20 11:37:13 +0200

    task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 class Square:
-    """My new class named square
-        Attributes:
-            none
-    """
+    '''My new class named square'''
     pass
-    """an empty block"""
+     '''an empty block'''
