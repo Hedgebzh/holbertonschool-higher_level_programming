3a899b466ac0f348349d35637e58a04133cdaab0
Hedgebzh <axel.gore35@gmail.com>
2022-09-20 14:54:28 +0200

    task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''module documentation'''
+
+
+class Square:
+    '''Class named square'''
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        area = self.__size**2
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print("\n", end="")
+        for i in range(self.size):
+            for i in range(self.size):
+                print("#", end="")
+            print(" ")
+
