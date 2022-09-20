89c8883b40e5f068d7d0cdfb483b371d72a15ff9
Hedgebzh <axel.gore35@gmail.com>
2022-09-20 12:03:56 +0200

    fix task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
     '''Class named square'''
     def __init__(self, size=0):
         self.__size = size
-        if size == isinstance(size, int):
+        if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
