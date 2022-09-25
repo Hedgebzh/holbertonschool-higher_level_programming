1b20686ecb9ebaa271c9b3c7ecf235c80edd8942
Hedgebzh <axel.gore35@gmail.com>
2022-09-25 18:26:08 +0200

    fix task 4

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -48,8 +48,9 @@ class Rectangle:
         for i in range(self.__height):
             for j in range(self.__width):
                 rectangle.append("#")
-            rectangle.append("\n")
-        return ("".join(rectangle))[:-1]
+                if i != self.__height - 1:
+                    rectangle.append("\n")
+        return ("".join(rectangle))
 
     def __repr__(self):
         new_rectangle = "Rectangle (" + str(self.__width)
