9d0bae9944dee5de35877577894065429c60f7d9
Hedgebzh <axel.gore35@gmail.com>
2022-09-29 15:04:00 +0200

    task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class checking"""
+
+
+def is_same_class(obj, a_class):
+    """check if object is same class"""
+    if isinstance(obj, a_class):
+        return True
+    return False
