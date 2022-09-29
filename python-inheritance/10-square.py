26f1ec2529816db812126a786dd541f8b6cc8efb
Hedgebzh <axel.gore35@gmail.com>
2022-09-29 15:47:55 +0200

    task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -8,5 +8,5 @@ class Square(Rectangle):
     def __init__(self, size):
         """init function"""
         self.size = size
-        super().init(self, size)
+        super().__init__(self, size)
         self.integer_validator("size", size)
