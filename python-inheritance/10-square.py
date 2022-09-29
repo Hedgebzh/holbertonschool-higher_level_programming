8b7fbd170feeb89542b85802442d191db41ee3e1
Hedgebzh <axel.gore35@gmail.com>
2022-09-29 15:45:50 +0200

    task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,8 +2,10 @@
 """new class named square"""
 Rectangle = __import__('9-rectangle.py').Rectangle
 
+
 class Square:
     """new class named square"""
     def __init__(self, size):
+        """init function"""
         self.size = size
         self.integer_validator("size", size)
