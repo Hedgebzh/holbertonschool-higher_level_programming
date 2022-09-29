a7e576bab88e61f1dc9ef49c73313a4b793178f9
Hedgebzh <axel.gore35@gmail.com>
2022-09-29 15:47:23 +0200

    task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -3,7 +3,7 @@
 Rectangle = __import__('9-rectangle.py').Rectangle
 
 
-class Square:
+class Square(Rectangle):
     """new class named square"""
     def __init__(self, size):
         """init function"""
