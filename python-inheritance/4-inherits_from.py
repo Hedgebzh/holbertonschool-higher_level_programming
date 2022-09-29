738cbe625f03f1b07b06611e004914e7b09154f8
Hedgebzh <axel.gore35@gmail.com>
2022-09-29 15:08:44 +0200

    task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class checking"""
+
+
+def inherits_from(obj, a_class):
+    """check if object is same class"""
+
+    if issubclass(type(obj), a_class):
+        return True
+    return False
