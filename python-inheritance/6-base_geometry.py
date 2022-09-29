3fa855bb5f16cf4340e41c3061590ed4e1b98f4b
Hedgebzh <axel.gore35@gmail.com>
2022-09-29 15:17:08 +0200

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""new class named Basegeometry"""
+
+
+class BaseGeometry:
+    """new class named BaseGeometry"""
+    def area(self):
+        """not implemented"""
+        raise Exception("area() is not implemented")
+
