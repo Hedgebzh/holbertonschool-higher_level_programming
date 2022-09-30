4a174c25009e096af3242516241616172b68fb09
Hedgebzh <axel.gore35@gmail.com>
2022-09-30 14:43:06 +0200

    task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""append write module"""
+
+
+def append_write(filename="", text=""):
+    """function for append write in a file"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
