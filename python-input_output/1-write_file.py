d81ce122d47f75ef231bea3b8da6cae4327e1f5e
Hedgebzh <axel.gore35@gmail.com>
2022-09-30 14:40:46 +0200

    task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""write file module"""
+
+
+def write_file(filename="", text=""):
+    """function for write in a file"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
