75c0579969c842110c30421b41cafbe5fa21cf3a
Hedgebzh <axel.gore35@gmail.com>
2022-10-03 10:49:28 +0200

    fix import base in rectangle module

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,10 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def size(self):
