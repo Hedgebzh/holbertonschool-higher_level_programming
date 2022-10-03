b1816f44008f1f0f029c4acd7ee22e17a5461658
Hedgebzh <axel.gore35@gmail.com>
2022-10-03 10:30:43 +0200

    add document for init function

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """Base module for almost a circle"""
 
-
 class Base:
     """Base class for almost a circle"""
     __nb_objects = 0
     def __init__(self, id=None):
         """Init function of the class"""
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
