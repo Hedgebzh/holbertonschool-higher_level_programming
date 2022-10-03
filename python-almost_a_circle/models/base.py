73c9971a3f59d7a66f32ee943e5eff21660e879a
Hedgebzh <axel.gore35@gmail.com>
2022-10-03 10:27:55 +0200

    add mains, tasks and tests

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Base module for almost a circle"""
+
+
+class Base:
+    """Base class for almost a circle"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
