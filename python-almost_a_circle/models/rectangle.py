9b171b55bcc28f86892774f9e914879b6d764578
Hedgebzh <axel.gore35@gmail.com>
2022-10-03 14:17:56 +0200

    fix check int

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -15,11 +15,11 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        return(self.__width)
+        return (self.__width)
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError ("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -27,11 +27,11 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        return(self.__height)
+        return (self.__height)
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError ("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -39,11 +39,11 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        return(self.__x)
+        return (self.__x)
 
     @x.setter
     def x(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError ("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -51,11 +51,11 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        return(self.__y)
+        return (self.__y)
 
     @y.setter
     def y(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError ("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
