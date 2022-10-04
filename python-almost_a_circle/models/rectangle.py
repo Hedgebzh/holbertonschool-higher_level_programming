fdce49d64e8d068dace8fb3e5ad721cab4957173
Hedgebzh <axel.gore35@gmail.com>
2022-10-04 15:06:06 +0200

    add update function

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -82,3 +82,20 @@ class Rectangle(Base):
         string += str(self.id) + ") " + str(self.__x) + "/" + str(self.__y)
         string += " - " + str(self.__width) + "/" + str(self.__height)
         return (string)
+
+    def update(self, *args):
+        """Update function for define new attributes"""
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
