34fb094d1da2e95dad895b95e42af6fa82fda46a
Hedgebzh <axel.gore35@gmail.com>
2022-10-04 15:30:55 +0200

    add square class

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square module for almost a circle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                  self.width))
+
