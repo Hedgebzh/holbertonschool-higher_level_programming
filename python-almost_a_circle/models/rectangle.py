27de212e740f59851f38f49bd8255eca5e3f81b0
Hedgebzh <axel.gore35@gmail.com>
2022-10-04 15:46:25 +0200

    add kwargs option for update function

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -78,24 +78,37 @@ class Rectangle(Base):
             print("")
 
     def __str__(self):
-        string = "[" + str(self.__class__.__name__) + "] " + "("
-        string += str(self.id) + ") " + str(self.__x) + "/" + str(self.__y)
-        string += " - " + str(self.__width) + "/" + str(self.__height)
-        return (string)
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
+                                                         self.width, self.height
+                                                        ))
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Update function for define new attributes"""
         if args and len(args) != 0:
-            a = 0
+            i = 0
             for arg in args:
-                if a == 0:
+                if i == 0:
                     self.id = arg
-                elif a == 1:
+                elif i == 1:
                     self.width = arg
-                elif a == 2:
+                elif i == 2:
                     self.height = arg
-                elif a == 3:
+                elif i == 3:
                     self.x = arg
-                elif a == 4:
+                elif i == 4:
                     self.y = arg
-                a += 1
+                i += 1
+                
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height == value
+                elif key == "x":
+                    self.x == value
+                elif key == "y":
+                    self.y = value
+
