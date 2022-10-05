df66a3827e9b4f9c1b45cfbbbc56312ee0b4ccd5
Hedgebzh <axel.gore35@gmail.com>
2022-10-05 11:22:50 +0200

    fix tests

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -1,43 +1,7 @@
 #!/usr/bin/python3
 "Unit tests for Rectangle class"
 import unittest
-from unittest import mock
-import io
 from models.square import Square
-
-
-class TestSquare(unittest.TestCase):
-  """Testing Square"""
-  def test_instance(self):
-      """test input size correct standard """
-      s = Square(5)
-      self.assertEqual(s.size, 5)
-  def test_area(self):
-      """testing area"""
-
-      s = Square(5)
-      self.assertEqual(s.area(), 25)
-
-      s = Square(1, 2)
-      self.assertEqual(s.area(), 1)
-  def test_display(self):
-      """test display()"""
-      s = Square(5)
-      with mock.patch("sys.stdout", new=io.StringIO()) as mock_stdout:
-        s.display()
-
-      assert mock_stdout.getvalue() == "#####\n#####\n#####\n#####\n#####\n"
-
-      s = Square(1, 2)
-      with mock.patch("sys.stdout", new=io.StringIO()) as mock_stdout:
-        s.display()
-
-      assert mock_stdout.getvalue() == "  #\n"
-  def test_string(self):
-      """Test str"""
-
-      s = Square(1, 2)
-      self.assertEqual(s.__str__(), '[Square] (11) 2/0 - 1')
 
 class TestSquare_update_args(unittest.TestCase):
     """unittests for testing update function"""
@@ -57,20 +21,20 @@ class TestSquare_update_args(unittest.TestCase):
     def test_update_two_args(self):
       """Test update function with two args"""
       s = Square(5, 5, 5, 5)
-      s.update(89, 1)
-      self.assertEqual("[Square] (89) 5/5 - 1", str(s))
+      s.update(89, 2)
+      self.assertEqual("[Square] (89) 5/5 - 2", str(s))
 
     def test_update_three_args(self):
       """Test update function with three args"""
       s = Square(5, 5, 5, 5)
-      s.update(89, 1, 2)
-      self.assertEqual("[Square] (89) 2/5 - 1", str(s))
+      s.update(89, 2, 3)
+      self.assertEqual("[Square] (89) 5/5 - 2", str(s))
 
     def test_update_four_args(self):
       """Test update function with four args"""
       s = Square(5, 5, 5, 5)
-      s.update(89, 1, 2, 3)
-      self.assertEqual("[Square] (89) 2/3 - 1", str(s))
+      s.update(89, 2, 3, 4)
+      self.assertEqual("[Square] (89) 4/5 - 2", str(s))
 
 
 
