370abb472f310e8de804e13bf9c263c12c9ab6c2
Hedgebzh <axel.gore35@gmail.com>
2022-10-05 10:30:30 +0200

    add tests

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -39,31 +39,34 @@ class TestSquare(unittest.TestCase):
       s = Square(1, 2)
       self.assertEqual(s.__str__(), '[Square] (11) 2/0 - 1')
 
-  def test_update_zero_args(self):
+class TestSquare_update_args(unittest.TestCase):
+    """unittests for testing update function"""
+
+    def test_update_zero_args(self):
       """Test update function with zero args"""
       s = Square(5, 5, 5, 5)
       s.update()
       self.assertEqual("[Square] (1) 5/5 - 5", str(s))
 
-  def test_update_one_args(self):
+    def test_update_one_args(self):
       """Test update function with one args"""
       s = Square(5, 5, 5, 5)
       s.update(89)
       self.assertEqual("[Square] (89) 5/5 - 5", str(s))
 
-  def test_update_two_args(self):
+    def test_update_two_args(self):
       """Test update function with two args"""
       s = Square(5, 5, 5, 5)
       s.update(89, 1)
       self.assertEqual("[Square] (89) 5/5 - 1", str(s))
 
-  def test_update_three_args(self):
+    def test_update_three_args(self):
       """Test update function with three args"""
       s = Square(5, 5, 5, 5)
       s.update(89, 1, 2)
       self.assertEqual("[Square] (89) 2/5 - 1", str(s))
 
-  def test_update_four_args(self):
+    def test_update_four_args(self):
       """Test update function with four args"""
       s = Square(5, 5, 5, 5)
       s.update(89, 1, 2, 3)
