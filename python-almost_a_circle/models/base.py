c494e5d658382569a5579a64409beda627c654d3
Hedgebzh <axel.gore35@gmail.com>
2022-10-07 15:33:40 +0200

    add static method from json string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,3 +22,9 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return("[]")
         return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or json_string == []:
+            return("[]")
+        return json.loads(json_string)
