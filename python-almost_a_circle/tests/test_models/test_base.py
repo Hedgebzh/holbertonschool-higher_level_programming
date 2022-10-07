3c22c8b30a582fa4d6c00be1f604bb14b8bb3c74
Hedgebzh <axel.gore35@gmail.com>
2022-10-07 14:13:30 +0200

    add tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -5,21 +5,20 @@ from models.base import Base
 
 
 class TestBase(unittest.TestCase):
-    "Unit tests suite for Base class"
+    """Unit tests suite for Base class"""
 
     def test_constantId(self):
-        "Test of Base for correctly initializing an id"
+        """Test of Base for correctly initializing an id"""
         b = Base(5)
         self.assertEqual(b.id, 5)
 
     def test_autoId(self):
-        "Test of Base for automatically assigning and incrementing an id"
+        """Test of Base for automatically assigning and incrementing an id"""
         b = Base()
         self.assertEqual(b.id, 1)
 
     def test_string(self):
         """Test of Base for case input is string"""
-        Base._Base_nb_objects = 0
         b = Base("string")
         self.assertEqual(b.id, "string")
 
