36b9cb532a3e3d6bff590d00c02662b519b9c556
Hedgebzh <axel.gore35@gmail.com>
2022-10-07 14:24:37 +0200

    fix executable

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -65,3 +65,4 @@ class TestBase(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+test
--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -149,3 +149,4 @@ class TestSquare(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+    
