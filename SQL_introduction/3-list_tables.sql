e7e3379383e8bed38d79345476cb836b1e95e956
Hedgebzh <axel.gore35@gmail.com>
2022-11-03 16:49:47 +0100

    task3

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,4 +1,4 @@
 -- Task description : Write a script that lists all the tables of a database in your MySQL server.
 
--- command for show and list all database on the server
+-- command for show all tables in the actual database
 SHOW tables;
