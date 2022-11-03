b991166a3897501460b164b2bff3d2413404e40f
Hedgebzh <axel.gore35@gmail.com>
2022-11-03 17:30:30 +0100

    task6

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,4 @@
 -- Task description : Write a script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
 
 -- command for show and list all database on the server
+INSERT INTO first_table VALUES('89', 'Best School');
