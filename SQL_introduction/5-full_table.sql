f2bbccf94303a98c858ae8690a29179ce083ed01
Hedgebzh <axel.gore35@gmail.com>
2022-11-03 17:34:09 +0100

    task7

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- Task description : Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 
--- command for show and list all database on the server
+-- command for create a table into show full description of table itself
 SHOW CREATE TABLE first_table;
--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,4 +1,4 @@
 -- Task description : Write a script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
 
--- command for show and list all database on the server
+-- list all rows of the table called
 SELECT * FROM first_table;
