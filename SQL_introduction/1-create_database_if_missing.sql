228bd871a794cfdba3647c3dbf4ea67286cb4efa
Hedgebzh <axel.gore35@gmail.com>
2022-11-03 16:10:00 +0100

    task1

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,4 @@
 -- Task description : Write a script that creates the database hbtn_0c_0 in your MySQL server.
 
 -- command for create database
-CREATE DATABASE hbtn_0c_0;
 CREATE DATABASE IF EXISTS hbtn_0c_0;
