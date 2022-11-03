3a47ad81b7ab18ea938d957014a7a8d5d2cb63ec
Hedgebzh <axel.gore35@gmail.com>
2022-11-03 16:22:48 +0100

    task2

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,3 +1,4 @@
 -- Task description : Write a script that deletes the database hbtn_0c_0 in your MySQL server.
 
--- command for show and list all database on the server
+-- command for remove database
+DROP DATABASE hbtn_0c_0;
