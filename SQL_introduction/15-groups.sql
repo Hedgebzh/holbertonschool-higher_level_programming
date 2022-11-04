e2914cc8621efc9ae0f14340c455da965d6270e4
Hedgebzh <axel.gore35@gmail.com>
2022-11-04 14:05:01 +0100

    task 15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- Task description : Write a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
 
 -- command for show and list all database on the server
-SELECT score, COUNT(*) AS number FROM second_table ORDER BY score DESC GROUP BY score ;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY score DESC;
