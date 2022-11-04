8cde143b037836b281639a05fd49f4dbfc54b65c
Hedgebzh <axel.gore35@gmail.com>
2022-11-04 09:49:42 +0100

    task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
 -- Task description : Write a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
 
 -- command for show and list all database on the server
-SELECT COUNT(id) FROM first_table WHERE id = 89;
+SELECT COUNT(*) FROM first_table WHERE id=89;
