46f33a227433be9b49f90d76fc07c7a7c591700e
Hedgebzh <axel.gore35@gmail.com>
2022-11-04 13:39:44 +0100

    task 12

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,7 @@
 -- Task description : Write a script that updates the score of Bob to 10 in the table second_table.
 
 -- command for show and list all database on the server
+UPDATE second_table
+SET
+	score = '10'
+WHERE id = 4;
