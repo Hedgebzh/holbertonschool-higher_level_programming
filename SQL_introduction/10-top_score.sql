9f74670881150c451ff6c1745e877ba1faa009bd
Hedgebzh <axel.gore35@gmail.com>
2022-11-04 10:22:07 +0100

    task 10

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,5 +1,4 @@
 -- Task description : Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
 
 -- command for show and list all database on the server
-
-SELECT (score, name) FROM second_table WHERE score >= 10 ORDER BY (score);
+SELECT score, name FROM second_table ORDER BY (score);
