e9dc5933b35dacc105eba37c052a18aa22790ee0
Hedgebzh <axel.gore35@gmail.com>
2022-11-04 13:51:44 +0100

    task 14

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- Task description : Write a script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
 
 -- command for show and list all database on the server
-SELECT AVG(average) FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
