e8046a98112fb8f9bceefc41555a7b4af8b37fa3
Hedgebzh <axel.gore35@gmail.com>
2022-11-04 14:17:28 +0100

    task 16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- Task description : Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
 
 -- command for show and list all database on the server
-SELECT score WHERE name IS NOT NULL;
+SELECT * WHERE name IS NOT NULL;
 ORDER BY score DESC;
