0a5cdeb455445740e6172cce738456f0b8675234
Hedgebzh <axel.gore35@gmail.com>
2022-11-07 16:07:25 +0100

    task 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,6 @@
 -- Task description : Write a script that creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- test
 CREATE USER IF NOT EXISTS 'user_0d_2'@locasthost IDENTIFIED BY 'user_0d_2_pwd';
+-- test
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@locasthost;
