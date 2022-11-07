d6842d05255a9b2ed07b1566972fd36e147ead87
Hedgebzh <axel.gore35@gmail.com>
2022-11-07 14:28:25 +0100

    task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 --Task description : Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 
+CREATE USER [IF NOT EXISTS] 'user_0d_1', 'user_0d_2';
 SHOW GRANTS [FOR user_0d_1, user_0d_2];
