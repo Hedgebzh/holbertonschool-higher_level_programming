d6f96f65eedd897f33b8ba5e376b59d61c0c0d85
Hedgebzh <axel.gore35@gmail.com>
2022-11-07 15:32:15 +0100

    task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Task description : Write a script that creates the MySQL server user user_0d_1.
-CREATE USER [IF NOT EXISTS] 'user_0d_1' IDENTIFIED BY PASSWORD 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY PASSWORD 'user_0d_1_pwd';
 GRANT ALL TO 'user_0d_1'@'localhost';
