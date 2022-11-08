8e80be0f10bba8495782fba173fc8866ab444c20
Hedgebzh <axel.gore35@gmail.com>
2022-11-08 11:44:18 +0100

    task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Task description : Write a script that lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.id, cities.name, states.name FROM states FULL JOIN cities;
+SELECT cities.id, cities.name, states.name FROM states INNER JOIN cities ON states.id = cities.state_id ORDER BY cities.id;
