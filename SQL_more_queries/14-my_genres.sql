3e7d4b47189dc1dbd43678730b8e56eb60687121
Hedgebzh <axel.gore35@gmail.com>
2022-11-13 17:36:25 +0100

    finals takls

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,6 @@
 -- Task description : Write a script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
-SELECT tv_genres.name AS name
-FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.name
-WHERE tv_show_genres.show_id = 8
-ORDER BY name;
+SELECT tv_genres.name FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title='Dexter'
+ORDER BY tv_genres.name;
--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,3 +1,6 @@
 -- Task description : Write a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
-CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY PASSWORD 'user_0d_1_pwd';
-GRANT ALL TO 'user_0d_1'@'localhost';
+SELECT title, name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title ASC, name ASC;
