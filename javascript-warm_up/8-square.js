daa8d9f30448a8774b3f2a8a85f2e8464ba78b5e
Hedgebzh <axel.gore35@gmail.com>
2022-12-09 17:19:15 +0100

    task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+let i = 0;
+let y = 0;
+let square = '';
+
+while (i < process.argv[2]) {
+  square += 'X';
+  i++;
+}
+
+while (y < process.argv[2]) {
+  console.log(square);
+  y++;
+}
+
