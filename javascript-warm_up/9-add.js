323b13d649917cbd45f5ba8a2d6fcf65c379abda
Hedgebzh <axel.gore35@gmail.com>
2022-12-09 17:39:37 +0100

    task 9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+process.argv[2] = process.argv[2] * 1;
+process.argv[3] = process.argv[3] * 1;
+
+function add(a, b) {
+  c = a + b;
+  return (c);
+}
+
+console.log(add(process.argv[2], process.argv[3]));
