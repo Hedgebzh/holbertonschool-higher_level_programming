abb99f53b24b1ae9ad16e715fe445a32e24c19e5
Hedgebzh <axel.gore35@gmail.com>
2022-12-09 13:48:18 +0100

    task 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,8 +2,8 @@
 
 if (process.argv.length > 3) {
   console.log('Arguments found');
-} else if (process.argv.length == 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
-  console.log('No argument')
+  console.log('No argument');
 }
