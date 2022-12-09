43596e6eb546820c92535277582a3c12a9ba42d8
Hedgebzh <axel.gore35@gmail.com>
2022-12-09 16:44:40 +0100

    task 6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-someArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const someArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 
-while (someArray[i] != null)
-{
+while (someArray[i] != null) {
   console.log(someArray[i]);
   i++;
 }
