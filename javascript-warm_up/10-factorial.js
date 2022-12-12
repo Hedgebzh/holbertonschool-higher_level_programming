42890473ba1123c3f00cec77545025588a5edf09
Hedgebzh <axel.gore35@gmail.com>
2022-12-12 17:03:14 +0100

    task 10 and 11

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -3,10 +3,9 @@
 process.argv[2] = process.argv[2] * 1;
 
 function factorial (a) {
-  if(a > 0) {
-    return (a * factorial(a - 1))
-  }
-   else {
+  if (a > 0) {
+    return (a * factorial(a - 1));
+  } else {
     return (1);
   }
 }
--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,27 +2,27 @@
 
 let i = 0;
 let j = 0;
-let biggest_n = 0;
-let second_biggest_n = 0;
+let BiggestN = 0;
+let SecondBiggestN = 0;
 
 if (process.argv[2] === undefined) {
   console.log(0);
 } else if (process.argv[3] === undefined) {
   console.log(0);
 } else {
-    while (process.argv[i] != undefined) {
-      process.argv[i] = process.argv[i] * 1;
-      if (process.argv[i] > biggest_n) {
-        biggest_n = process.argv[i];
-      }
-      i++;
+  while (process.argv[i] !== undefined) {
+    process.argv[i] = process.argv[i] * 1;
+    if (process.argv[i] > BiggestN) {
+      BiggestN = process.argv[i];
     }
-    while (process.argv[j] != undefined) {
-        process.argv[j] = process.argv[j] * 1;
-        if (process.argv[j] < biggest_n && process.argv[j] > second_biggest_n) {
-            second_biggest_n = process.argv[j];
-        }
-        j++
+    i++;
+  }
+  while (process.argv[j] !== undefined) {
+    process.argv[j] = process.argv[j] * 1;
+    if (process.argv[j] < BiggestN && process.argv[j] > SecondBiggestN) {
+      SecondBiggestN = process.argv[j];
     }
+    j++;
+  }
+  console.log(SecondBiggestN);
 }
-console.log(second_biggest_n);
