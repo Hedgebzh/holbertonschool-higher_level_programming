2924cbe19634d238bccc1e05668a12d58a8a1d66
Hedgebzh <axel.gore35@gmail.com>
2022-12-12 15:47:55 +0100

    task 10 & 11

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+process.argv[2] = process.argv[2] * 1;
+
+function factorial (a) {
+  if(a > 0) {
+    return (a * factorial(a - 1))
+  }
+   else {
+    return (1);
+  }
+}
+
+console.log(factorial(process.argv[2]));
--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+if (process.argv[2] === undefined) {
+    console.log(0);
+} else if (process.argv[3] === undefined) {
+    console.log(0);
+}
+
+let i = 3;
+biggest_n = process.argv[2];
+
+while (process.argv[i] != undefined) {
+    if (process.argv[i] > process.argv[i - 1]) {
+    biggest_n = process.argv[i];
+    }
+    i++;
+  }
+  
+  console.log(biggest_n);
