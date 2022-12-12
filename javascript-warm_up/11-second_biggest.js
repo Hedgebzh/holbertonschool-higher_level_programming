92c0776bcc79659f0b9d0a5cbac2327c45215add
Hedgebzh <axel.gore35@gmail.com>
2022-12-12 16:55:44 +0100

    task 11 and fix semistandard for task 10

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,28 @@
 #!/usr/bin/node
 
+let i = 0;
+let j = 0;
+let biggest_n = 0;
+let second_biggest_n = 0;
+
 if (process.argv[2] === undefined) {
   console.log(0);
 } else if (process.argv[3] === undefined) {
   console.log(0);
-}
-
-let i = 0;
-biggest_n = process.argv[2];
-
-while (process.argv[i] != undefined) {
-    process.argv[i] = process.argv[i] * 1;
-    if (process.argv[i] > biggest_n) {
-      biggest_n = process.argv[i];
+} else {
+    while (process.argv[i] != undefined) {
+      process.argv[i] = process.argv[i] * 1;
+      if (process.argv[i] > biggest_n) {
+        biggest_n = process.argv[i];
+      }
+      i++;
     }
-    i++;
-  }
-  
-  console.log(biggest_n);
+    while (process.argv[j] != undefined) {
+        process.argv[j] = process.argv[j] * 1;
+        if (process.argv[j] < biggest_n && process.argv[j] > process.argv[j - 1]) {
+            second_biggest_n = process.argv[j];
+        }
+        j++
+    }
+}
+console.log(second_biggest_n);
