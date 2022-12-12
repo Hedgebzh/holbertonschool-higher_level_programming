274b384d43f2518269bf155cbde59f740643fb3f
Hedgebzh <axel.gore35@gmail.com>
2022-12-12 16:27:33 +0100

    fix task 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 
 if (process.argv[2] === undefined) {
-    console.log(0);
+  console.log(0);
 } else if (process.argv[3] === undefined) {
-    console.log(0);
+  console.log(0);
 }
 
-let i = 3;
+let i = 0;
 biggest_n = process.argv[2];
 
 while (process.argv[i] != undefined) {
+    process.argv[i] = process.argv[i] * 1;
     if (process.argv[i] > biggest_n) {
-    biggest_n = process.argv[i];
+      biggest_n = process.argv[i];
     }
     i++;
   }
