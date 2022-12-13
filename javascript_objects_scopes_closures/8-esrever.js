9f33162192b1bda2dda8e37e7123e29e62a6aad1
Hedgebzh <axel.gore35@gmail.com>
2022-12-13 16:29:47 +0100

    task 8

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const ListReverse = [];
+  let ListLength = list.length - 1;
+
+  while (ListLength >= 0) {
+    ListReverse.push(list[ListLength]);
+    ListLength--;
+  }
+  return (ListReverse);
+};
