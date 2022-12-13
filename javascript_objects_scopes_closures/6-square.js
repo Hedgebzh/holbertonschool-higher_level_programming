9cdd0609533d00215c1341ce88241de4f8853aef
Hedgebzh <axel.gore35@gmail.com>
2022-12-13 15:45:55 +0100

    task 6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+  charPrint(c) {
+    let i = 0;
+    let y = 0;
+    let PrintSquare = '';
+
+    if (c === undefined) {
+      c = 'X';
+    }
+    while (i < this.size) {
+      while (y < this.size) {
+      PrintSquare += c
+      y++
+      }
+      console.log(PrintSquare)
+      i++
+    }
+  }
+};
