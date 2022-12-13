a944abd02cd174c5a9cc2b487bb122bc40673c62
Hedgebzh <axel.gore35@gmail.com>
2022-12-13 13:46:55 +0100

    task 2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return (undefined);
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
