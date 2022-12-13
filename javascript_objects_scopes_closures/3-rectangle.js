f9fa628f07ca432e124c05e3a1d909768e9a1dee
Hedgebzh <axel.gore35@gmail.com>
2022-12-13 14:17:55 +0100

    task 3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,41 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return (undefined);
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+  let i = 0
+  let y = 0
+
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
+    }
+  }
+}
+  
+  
+  
+  /* print() {
+
+    let i = 0
+    let y = 0
+    let PrintRectangle = '';
+
+    while (i < this.height) {
+      while (y < this.weight) {
+        PrintRectangle += 'X';
+        y++;
+      }
+      console.log(PrintRectangle);
+      i++;
+    }
+  }
+}; */
+
