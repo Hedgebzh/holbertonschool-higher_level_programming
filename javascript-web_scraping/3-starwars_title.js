4eac0b1524b77c26fdec0d90f157a05eb3273bb7
Hedgebzh <axel.gore35@gmail.com>
2022-12-15 15:56:11 +0100

    task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const request = require('request');
+const UrlConcat = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+function callback (error, response, body) {
+  if (error != null) {
+    console.log(error);
+  }
+  console.log(JSON.parse(body).title);
+}
+
+request(UrlConcat, callback);
