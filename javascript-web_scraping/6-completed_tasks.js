f75bed1189cb5f625159b7417912c34306833ca9
Hedgebzh <axel.gore35@gmail.com>
2022-12-16 21:05:38 +0100

    task 4 5 +

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    const all = JSON.parse(body);
+    const TaskCompleted = {};
+    for (let i = 0; i < todos.length; i++) {
+      if (all[i].completed === true) {
+        if (TaskCompleted[todos[i].userId] === undefined) {
+          TaskCompleted[todos[i].userId] = 0;
+        }
+        TaskCompleted[all[i].userId]++;
+      }
+    }
+    console.log(TaskCompleted);
+  }
+});
